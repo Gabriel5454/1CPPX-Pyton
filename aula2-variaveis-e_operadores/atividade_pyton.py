20e10abdd4a460ba10bcd7a0b3f7c91728f09a65
Gabriel5454 <11gabriel110211@gmail.com>
2026-03-19 22:28:00 -0300

    primeiro commit

---
+++ aula2-variaveis-e_operadores/atividade_pyton.py
@@ -0,0 +1,31 @@
+print('Olá mundo')
+
+print(7 +4)
+print('7 + 4')
+print('7 ' + '4') # Contatenação de STRINGS
+# Comentários de 1 linha
+'''
+comenrarios de 
+mutiplas linhas
+'''
+
+# variaveis
+nome = "Gabriel" # string
+idade = 18 # int
+peso = 55.6 # float
+
+print(nome, idade, peso)
+print(f"o meu nome é {nome} e tenho {idade} anos")
+
+# INPUTS - simulação de FORMS   do cms
+nome = input("Digite o seu nome: ")
+
+num = int(input("Digite a sua idade: "))
+nova_Idade = idade + 1
+print(nome, idade)
+print(idade + 1)
+print(nova_Idade)
+
+
+
+
