43d5f892dd0be832fae4715abb85ee220ccd4ffc
Gabriel5454 <11gabriel110211@gmail.com>
2026-04-23 22:35:46 -0300

    aula04

---
+++ aula4-repeticao/aula04-for.py
@@ -0,0 +1,17 @@
+# for cont_music in range(3):
+#    print(f"Música: {cont_music}")
+#
+# # de 1 até 11, pulando  de 2 e 2
+# for i in range(1, 12, 2):
+#     print(i)
+#
+# # Atividade 3
+# qtd_music = int(input("Digite a quantidade de musicas que tem na playlist: "))
+#
+# for i in range(qtd_music):
+#     print(f"Música: {i}")
+
+#Rep. Encadeadas
+for i in range(0, 4):
+    for j in range(0, 3, 2):
+        print(f"i:{i}, j:{j}")
---
+++ aula4-repeticao/aula04-while.py
@@ -0,0 +1,12 @@
+cp = 0
+
+while cp < 10:
+    print("Produto: ")
+    cp += 1
+
+    if cp == 3:
+        continue
+
+
+    if cp == 7:
+        break
