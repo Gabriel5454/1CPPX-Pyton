deb8770cec642c1ac1060376bc3398d65298368e
Gabriel5454 <11gabriel110211@gmail.com>
2026-08-13 22:31:44 -0300

    dicionario e tupla

---
+++ aula_07/dicionario.py
@@ -0,0 +1,15 @@
+eng2esp = {}
+print(eng2esp)
+
+eng2esp['one'] = 'uno'
+print(eng2esp)
+
+eng2esp = {
+    'one': 'uno',
+    'two': 'dos',
+}
+print(eng2esp)
+
+print('one' in eng2esp)
+print('dos' in eng2esp)
+print('two' in eng2esp)
